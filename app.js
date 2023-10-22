2f185e64e17987b76b5ef42e5e0ce83edfffce2e
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-10-22 16:33:42 -0700

    finished slider function that takes a value from the user-submitted form

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 let sliderDollar = document.querySelectorAll('.dollar');
 let sliders = document.querySelectorAll('.slider');
+let userInput = document.querySelector('#user-input');
+let submitBtn = document.querySelector('#submit');
+let userInputValue;
 
+// submitBtn.addEventListener('click', function () {
+//   if (userInput) {
+//     userInputValue = parseInt(userInput.value);
+//     return userInputValue;
+//   }
+// });
+
+userInput.addEventListener('input', function () {
+  let userInputValue = userInput.value;
+  userInputValue = userInputValue.replace(/[^0-9.]/g, '');
+  console.log(userInputValue);
+
+  // Update dollar values for all sliders
+  sliders.forEach(function (slider, index) {
+    let dollarOutput = document.querySelector('#dollar-value' + (index + 1));
+    dollarOutput.innerHTML =
+      '$' + parseInt(userInputValue * (slider.value / 100));
+  });
+});
+
+// SLIDER FUNCTION
 sliders.forEach(function (slider, index) {
-  let output = document.querySelector('#value' + (index + 1));
-  output.innerHTML = slider.value + '%';
+  let percentOutput = document.querySelector('#percent-value' + (index + 1));
+  let dollarOutput = document.querySelector('#dollar-value' + (index + 1));
+  percentOutput.innerHTML = slider.value + '%';
+  dollarOutput.innerHTML =
+    '$' + parseInt(userInput.value * (slider.value / 100));
 
   slider.oninput = function () {
     let currentValue = parseInt(this.value);
@@ -16,9 +43,12 @@ sliders.forEach(function (slider, index) {
     if (totalValue > 100) {
       let excess = totalValue - 100;
       this.value = currentValue - excess;
-      output.innerHTML = this.value + '%';
+      percentOutput.innerHTML = this.value + '%';
     } else {
-      output.innerHTML = currentValue + '%';
+      percentOutput.innerHTML = currentValue + '%';
     }
+    // Update dollar values for this slider
+    dollarOutput.innerHTML =
+      '$' + parseInt(userInput.value * (slider.value / 100));
   };
 });
